e0930cac9bda913262c5d191d72a814447450ae3
Beta Pictoris <beta@ozx.me>
2023-05-13 11:12:46 -0700

    ✨ Added accordion

---
+++ src/stories/accordion/Accordion.stories.jsx
@@ -0,0 +1,11 @@
+import Accordion from "./Accordion";
+
+export default { title: "Accordion", component: Accordion };
+
+export const Default = {
+  args: {
+    title: "Apollo 13",
+    children:
+      "Apollo 13 was the seventh crewed mission in NASA's Apollo space program and was intended to be the third mission to land humans on the Moon. However, the mission encountered a critical failure that prevented the lunar landing and posed a severe threat to the lives of the astronauts.",
+  },
+};
