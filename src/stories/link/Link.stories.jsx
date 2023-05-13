700a4f4568f273871e1f1985ae7de9269343b731
Beta Pictoris <beta@ozx.me>
2023-05-13 10:46:11 -0700

    :sparkles: Added links

---
+++ src/stories/link/Link.stories.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "./Link";
+
+export default { title: "Link", component: Link };
+
+export const Default = {
+  args: {
+    href: "",
+    children: "Learn more",
+  },
+  argTypes: {
+    variant: {
+      options: ["default", "button", "arrow"],
+      control: { type: "select" },
+    },
+    target: {
+      options: ["_self", "_blank", "_parent", "_top"],
+      control: { type: "select" },
+    },
+  },
+};
