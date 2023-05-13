229c180e083305b78c956d39d0e5da93ad4b5732
Beta Pictoris <beta@ozx.me>
2023-05-13 09:45:30 -0700

    :truck: Complete commit fa4f438df962168b1e4f1204ed44184504680997

---
+++ src/stories/button/Button.stories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "./Button";
+
+export default { title: "Button", component: Button };
+
+export const Default = {
+  args: {
+    children: "Launch!",
+  },
+  argTypes: {
+    variant: {
+      options: ["primary", "secondary"],
+      control: { type: "select" },
+    },
+  },
+};
