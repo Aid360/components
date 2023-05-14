4f7a34ca4bc5d273779fb7ac812be86c83ffc9b6
Beta Pictoris <beta@ozx.me>
2023-05-13 19:54:32 -0700

    :pencil2: Fixed typo

--- src/stories/textinput/TextInput.stories.jsx
+++ src/stories/textinput/TextInput.stories.jsx
@@ -5,7 +5,7 @@ export default { title: "Text Input", component: TextInput };
 
 export const Default = {
   args: {
-    label: "Zodia symbol",
+    label: "Zodiac symbol",
     placeholder: "Your zodiac symbol (based off of your date of birth).",
     example: "Leo",
   },
