05927457566f5b18763cbdf31e90719a45c1cc33
Daniel <beta@ozx.me>
2023-05-15 21:01:38 +0000

    :sparkles: Added Persona

---
+++ src/components/Persona/Persona.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Persona.module.css";
+
+interface Props {
+  href: string
+  img: string
+  name: string
+}
+
+export default function Persona(props: Props) {
+  return (
+    <a href={props.href} className="persona">
+      <img src={props.img} alt="name" className="persona__img" />
+      <span className="persona__name">{props.name}</span>
+    </a>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Alert } from "./Alert";
 export { default as Badge } from "./Badge";
 export { default as Button } from "./Button";
 export { default as Link } from "./Link";
+export { default as Persona } from "./Persona";
