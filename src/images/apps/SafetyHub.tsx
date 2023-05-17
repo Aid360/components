8f0d2ad698c362641bc88b6ac3f78195bffd986f
Beta Pictoris <beta@ozx.me>
2023-05-16 17:02:52 -0700

    🍱 Added new app logo

--- src/images/apps/SafetyHub.tsx
+++ src/images/apps/SafetyHub.tsx
@@ -3,35 +3,63 @@ import React from "react";
 export default function SafetyHub() {
   return (
     <svg
-      width="128"
-      height="128"
-      viewBox="0 0 128 128"
+      width="230"
+      height="248"
+      viewBox="0 0 230 248"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M64.4069 3.70938L64 3.13977L63.5931 3.70938L13.5931 73.7094L13.3469 74.054L13.6464 74.3536L63.6464 124.354L64 124.707L64.3536 124.354L114.354 74.3536L114.653 74.054L114.407 73.7094L64.4069 3.70938Z"
-        fill="white"
-        stroke="white"
-      />
-      <mask
-        id="mask0_50_118"
-        maskUnits="userSpaceOnUse"
-        x="14"
-        y="4"
-        width="100"
-        height="120"
-      >
-        <path d="M64 4L114 74L64 124L14 74L64 4Z" fill="white" />
-      </mask>
-      <g mask="url(#mask0_50_118)">
-        <rect x="14" y="4" width="100" height="120" fill="#D9D9D9" />
-        <path d="M14 4H64V68.5L14 75V4Z" fill="#F0EFFF" />
-        <path d="M114 4H64V68.5L114 75V4Z" fill="#C1C0D2" />
-        <path d="M64 138H114V73.5L64 67V138Z" fill="#C1C0D2" />
-        <path d="M64 126H114V74L64 68.5V126Z" fill="#12111E" />
-        <path d="M64 126.5H14V74L64 68.5V126.5Z" fill="#1D1C2D" />
+      <g filter="url(#filter0_i_87_124)">
+        <path
+          d="M88.3855 56C88.3855 56 88.3855 20 124.386 20C160.386 20 160.386 56 160.386 56V95.5C160.386 95.5 89.3857 95.5 50.3855 95.5C11.3854 95.5 8.38538 152.5 50.3855 152.5"
+          stroke="#FFCB41"
+          stroke-width="12"
+          stroke-linecap="round"
+        />
+        <path
+          d="M140.62 191.5C140.62 191.5 140.62 227.5 104.62 227.5C68.6198 227.5 68.6198 191.5 68.6198 191.5V152C68.6198 152 139.62 152 178.62 152C217.62 152 220.62 95 178.62 95"
+          stroke="#FFCB41"
+          stroke-width="12"
+          stroke-linecap="round"
+        />
       </g>
+      <defs>
+        <filter
+          id="filter0_i_87_124"
+          x="14"
+          y="14"
+          width="201.005"
+          height="223.5"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect1_innerShadow_87_124"
+          />
+        </filter>
+      </defs>
     </svg>
   );
 }
